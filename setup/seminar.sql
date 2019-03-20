6dd43779407fa016469c65b467b8492bf876be65
lannstark <choi-0202@kaist.ac.kr>
2019-03-21 01:34:20 +0900

    Feature get seminars API, upload button style, Front file upload handling

--- setup/seminar.sql
+++ setup/seminar.sql
@@ -5,6 +5,7 @@
  *  - id : Primary Key
  *  - title : Seminar title
  *  - speaker: Seminar spekaer
+ *  - file_name: File name
  *  - created_date : Created date
  */
 
@@ -14,6 +15,7 @@ CREATE TABLE IF NOT EXISTS `seminars` (
   `id` tinyint(1) NOT NULL AUTO_INCREMENT,
   `title` varchar(30) NOT NULL,
   `speaker` varchar(30) NOT NULL,
+  `file_name` varchar(80) NOT NULL,
   `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
