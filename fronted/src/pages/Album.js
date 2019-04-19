eb93e1a474d6e952e25add5c650988f95c5c27b7
Ubuntu <ubuntu@ip-172-31-23-37.ap-northeast-2.compute.internal>
2019-04-19 04:57:45 +0000

    working

--- fronted/src/pages/Album.js
+++ fronted/src/pages/Album.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react"
+import { Card, Image } from "semantic-ui-react"
+
 import Navigation from "./components/Navigation"
 import "./../styles/album.scss"
+
 
 /**
  * Album
@@ -13,8 +16,44 @@ class Album extends Component {
       <React.Fragment>
       <Navigation />
       <div id="photo">
+        <div id="album_breadcrumb" className="album index">
+          <button className="ui red attached button album" id="newAlbum">Upload</button>
+          <div className="yellow rectangle" style={{width: "251px"}}>Album</div>
+          <div className="yellow triangle"></div>
+        </div>
 
-      앨범
+        <div id="album-card-grid">
+          <Card>
+            <Image alt="Image" src={"https://dev.sparcs.org/static/img_1234.jpeg"}/>
+            <Card.Header>2019년</Card.Header>
+            <Card.Meta>
+              <span className="data">3 Events, 17 Photos</span>
+            </Card.Meta>
+          </Card>
+
+          <Card>
+            <Image alt="Image" src={"https://dev.sparcs.org/static/img_1234.jpeg"}/>
+            <Card.Header>2019년</Card.Header>
+            <Card.Meta>
+              <span className="data">3 Events, 17 Photos</span>
+            </Card.Meta>
+          </Card>
+          <Card>
+            <Image alt="Image" src={"https://dev.sparcs.org/static/img_1234.jpeg"}/>
+            <Card.Header>2019년</Card.Header>
+            <Card.Meta>
+              <span className="data">3 Events, 17 Photos</span>
+            </Card.Meta>
+          </Card>
+          <Card>
+            <Image alt="Image" src={"https://dev.sparcs.org/static/img_1234.jpeg"}/>
+            <Card.Header>2019년</Card.Header>
+            <Card.Meta>
+              <span className="data">3 Events, 17 Photos</span>
+            </Card.Meta>
+          </Card>
+
+        </div>
       </div>
 
       </React.Fragment>
